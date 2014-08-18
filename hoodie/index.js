2b593ffc372c4fb4643bde8db537b3b62c0deb21
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-18 19:10:28 +1000

    rebuild

--- hoodie/index.js
+++ hoodie/index.js
@@ -1,12 +1,6 @@
 var fs = require('fs');
-// var deck = require('decker')();
 var s = require('shazam');
 
 s('Prototyping apps with Hoodie', [
   s.md(fs.readFileSync(__dirname + '/README.md', 'utf8'))
 ]);
-
-// deck.css(fs.readFileSync(__dirname + '/node_modules/decker/themes/basic.css'));
-// deck.css(fs.readFileSync(__dirname + '/node_modules/decker/themes/code/default.css'));
-// deck.add(fs.readFileSync('./README.md'));
-// document.body.appendChild(deck.render());
