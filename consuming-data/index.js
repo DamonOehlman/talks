74136cef73ae621945dccb25a02a4e33c0dc76c5
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-19 20:58:48 +1000

    simply require the slide now

--- consuming-data/index.js
+++ consuming-data/index.js
@@ -2,5 +2,5 @@ var fs = require('fs');
 var s = require('shazam');
 
 s('Liberating Data', [
-  s.md(fs.readFileSync(__dirname + '/prep/datasets.md', 'utf8'))
+  s.md(require('./importing-data.md'))
 ]);
