54dd8131cebbe41f26c93d9b813efe77acd93540
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-20 23:33:57 +1000

    Update to reflect changes in shazam

--- consuming-data/index.js
+++ consuming-data/index.js
@@ -2,5 +2,7 @@ var fs = require('fs');
 var s = require('shazam');
 
 s('Liberating Data', [
-  s.md(require('./importing-data.md'))
+  s.h1('Step 1: Importing Data'),
+  s.md(require('./importing-data.md')),
+  s.md(require('./parsing-data.md'))
 ]);
