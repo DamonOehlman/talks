07ac866583d1d4f976c11d807254b669e9945320
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-27 16:12:41 +1000

    Fix require statement

--- consuming-data/examples.js
+++ consuming-data/examples.js
@@ -1,8 +1,9 @@
 var h = require('hyperscript');
 var s = require('shazam');
+var snap = require('./lib/snapito');
 
 module.exports = [
   s.h1('Examples'),
-  snap('nationalmap.nicta.com.au'),
+  s.site('nationalmap.nicta.com.au', { png: 'national-map' }),
   snap('github.com/morganherlocker/voxel-openstreetmap')
 ];
