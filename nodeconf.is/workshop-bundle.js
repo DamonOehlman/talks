0f11f862cf533ee9c2685b7e59c4ac9ea4b51b82
Damon Oehlman <damon.oehlman@gmail.com>
2014-09-15 17:47:02 +0300

    Added debugging tips

--- nodeconf.is/workshop-bundle.js
+++ nodeconf.is/workshop-bundle.js
@@ -14775,7 +14775,10 @@ s({
     s.slide().h2('These slides').url('http://damonoehlman.github.io/talks/nodeconf.is/workshop.html'),
     s.slide().h2('Interactive Guidebook').url('http://guidebook.rtc.io/'),
     s.slide().h2('RequireBin is your friend').url('http://requirebin.com/?gist=d3059864c2cf08e44b44'),
-    s.slide().h2('Soup to Nuts Conferencing').url('https://github.com/rtc-io/rtcio-demo-quickconnect')
+    s.slide().h2('Soup to Nuts Conferencing').url('https://github.com/rtc-io/rtcio-demo-quickconnect'),
+    s.slide().h1('Debugging WebRTC'),
+    s.slide().h2('Chrome WebRTC Internals').url('chrome://webrtc-internals'),
+    s.slide().h2('Debugging WebRTC').url('https://gist.github.com/DamonOehlman/7329833')
   ]
 });
 
